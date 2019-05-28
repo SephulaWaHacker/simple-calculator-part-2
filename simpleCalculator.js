5ed69d05f42eb73cc4ddc1d1d01e201bc80f4ea9
decentguy <bludd1one@gmail.com>
2019-05-28 10:54:13 +0200

    added simpleCalculator.js & simpleCalculatorSpec.js

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -1,13 +1,22 @@
-function add() {
-	let sum = 0;
-	for (let i = 0; i < arguments.length; i++) 
-		sum += arguments[i];
-	return sum;
+class Calculator{
+	constructor(){
+		this.add = function() {
+			let sum = 0;
+			for (let i = 0; i < arguments.length; i++) 
+				sum += arguments[i];
+			return sum;
+		}
+
+		this.multiply = function () {
+			let sum = arguments[0];	
+			for (let i = 1; i < arguments.length; i++)
+				sum *= arguments[i];
+			return sum;
+		}
+	}
+
 }
 
-function multiply() {
-	let sum = arguments[0];	
-	for (let i = 1; i < arguments.length; i++)
-		sum *= arguments[i];
-	return sum;
-}
+const calculator = new Calculator();
+console.log(calculator.add(1,3));
+console.log(calculator.multiply(3,3));
--- simpleCalculatorSpec.js
+++ simpleCalculatorSpec.js
@@ -1,31 +1,33 @@
 describe("simple calculator", function() {
-
+  beforeEach(() => {
+    const calculator = new Calculator();
+  })
   it("should add two numbers", () => {
     let [arr1, arr2, arr3 ] = [[],[],[]];
     arr1.push(0,0);
     arr2.push(-1,-1);
     arr3.push(4,5);
-    expect(add(...arr1)).toEqual(0);
-    expect(add(...arr2)).toEqual(-2);
-    expect(add(...arr3)).toEqual(9);
+    expect(calculator.add(...arr1)).toEqual(0);
+    expect(calculator.add(...arr2)).toEqual(-2);
+    expect(calculator.add(...arr3)).toEqual(9);
   })
 
   it("should add as many numbers as I want", () => {
     let arr = [];
     arr.push(1,2,3,4,10);
-    expect(add(...arr)).toEqual(20);
+    expect(calculator.add(...arr)).toEqual(20);
   });
 
   it("should multiply 2 numbers", () => {
     let arr = [];
     arr.push(1,2);
-    expect(multiply(...arr)).toEqual(2)
+    expect(calculator.multiply(...arr)).toEqual(2)
   });
 
   it("should multiply as many numbers as I want", () => {
     let arr = [];
     arr.push()
-    expect(multiply(...arr)).toEqual()
+    expect(calculator.multiply(...arr)).toEqual()
   });
 
 });
